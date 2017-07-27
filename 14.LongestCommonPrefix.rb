9d7cf315743e231c38b5ed6d9a1d708cfffcb4b3
ceclinux <ceclinux@users.noreply.github.com>
2017-07-28 00:09:26 +0800

    fifth day 14.LongestCommonPrefix

---
+++ 14.LongestCommonPrefix.rb
@@ -0,0 +1,10 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+#
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+  return '' if strs.empty?
+  min, max = strs.minmax
+  idx = min.size.times{ |i| break i if min[i] != max[i] }
+  min[0...idx]
+end
