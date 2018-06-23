43c81e94038e4ccaa2d340230f84078361944f2d
ceclinux <ceclinux@users.noreply.github.com>
2018-06-24 00:40:42 +0800

    day 221th

--- The Little Schemer chapter 8.rkt
+++ The Little Schemer chapter 8.rkt
@@ -48,7 +48,7 @@
       (cond
         ((null? l) '())
         ((eq? (car l) old)
-         (seq new old (cdr l)))
+         (seq new old (cdr l))) 
         (else (cons (car l) ((insert-g seq) new old (cdr l))))))))
 
 (define seqL
@@ -58,3 +58,63 @@
 (define insertL2 (insert-g seqL))
 
 (insertL2  'wa 'ta '(cc ta jj))
+
+(define insertL3
+  (insert-g
+   (lambda (new old l)
+     (cons new (cons old l)))))
+
+(define seqS
+  (lambda (new old l)
+    (cons (new l))))
+
+(define subst (insert-g seqS))
+
+;用函数来抽象通用形式
+
+(define atom-to-function
+  (lambda (x)
+    (cond
+      ((eq? x '+) +)
+      ((eq? x '*) *)
+      (else exp))))
+
+(define atom? 
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define 1st-sub-exp
+  (lambda (aexp)
+       (car (cdr aexp))))
+
+(define 2st-sub-exp
+  (lambda (aexp)
+    (car (cdr (cdr aexp)))))
+
+(define operator
+  (lambda (aexp)
+(car aexp)))
+
+ (define value
+   (lambda (nexp)
+     (cond
+       ((atom? nexp) nexp)
+     (else ((atom-to-function (operator nexp)) (value (1st-sub-exp nexp)) (value (2st-sub-exp nexp)))))))
+
+(value '(* (+ 2 3) 5))
+
+(define eq?-tuna
+  (eq?-c (quote tuna)))
+
+(eq?-tuna 'tuna)
+
+(define multiremberT
+  (lambda (test?)
+    (lambda (lat)
+    (cond
+      ((null? lat) '())
+      ((test? (car lat))
+             ((multiremberT test?) (cdr lat)))
+      (else (cons (car lat) ((multiremberT test?) (cdr lat))))))))
+
+((multiremberT eq?-tuna) '(shrimp salad tuna salad and tuna))
