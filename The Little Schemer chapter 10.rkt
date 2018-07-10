20a414dcac7556091a7dd7b51eddeb0f6a365913
ceclinux <ceclinux@users.noreply.github.com>
2018-07-11 01:46:56 +0800

    day 231th and 232th

--- The Little Schemer chapter 10.rkt
+++ The Little Schemer chapter 10.rkt
@@ -1,5 +1,34 @@
 #lang racket
 
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define first
+  (lambda (p)
+    (cond
+      (else (car p)))))
+
+(define second
+  (lambda (p)
+    (cond
+      (else (car (cdr p))))))
+
+(define third
+  (lambda (p)
+    (cond
+      (else (cdr (cdr (cdr p)))))))
+
+(define build
+  (lambda (s1 s2)
+    (cond
+      (else (cons s1
+                  (cons s2 '()))))))
+
+(define extend-table cons)
+(define new-entry build)
+
 (define lookup-in-entry
   (lambda (name entry entry-f)
     (lookup-in-entry-help name
@@ -17,21 +46,21 @@
     )
   )
 
-(lookup-in-entry 'entree '((appetizer entree beverage) (food tastes good))
-                 (lambda (x) (x)))
+; (lookup-in-entry 'entree '((appetizer entree beverage) (food tastes good))
+; (lambda (x) (x)))
 
 (define lookup-in-table
   (lambda (name table table-f)
     (cond
       ((null? table) (table-f name))
       ((lookup-in-entry name (car table) (lambda (x) lookup-in-table x (cdr table)
-                                            table-f))))))
+                                           table-f))))))
 
 (lookup-in-table 'entree '(((entree dessert) (spaghetti spumoni)) ((appetizer
-                                                                   entree
-                                                                   beverage)
-                                                                 (food tastes
-                                                                       good)))
+                                                                     entree
+                                                                     beverage)
+                                                                   (food tastes
+                                                                         good)))
                  (lambda (x) (x)))
 
 (define expression-to-action
@@ -63,11 +92,11 @@
       ((atom? (car e))
        (cond
          ((eq? (car e) (quote quote))
-         *quote)
+          *quote)
          ((eq? (car e) (quote lambda))
-         *lambda)
-         ((eq? (car e) (quote lambda))
-         *cond)
+          *lambda)
+         ((eq? (car e) (quote cond))
+          *cond)
          (else *application)))
       (else *application))))
 
@@ -78,3 +107,118 @@
 (define meaning
   (lambda (e table)
     ((expression-to-action e) e table)))
+
+(define *const
+  (lambda (e table)
+    (cond
+      ((number? e) e)
+      ((eq? e #t) #t)
+      ((eq? e #f) #f)
+      (else (build (quote primitive) e)))))
+
+(define *quote
+  (lambda (e table)
+    (text-of e)))
+
+(define text-of second)
+
+(define *identifier
+  (lambda (e table)
+    (lookup-in-table e table initial-table)))
+
+(define initial-table
+  (lambda (name)
+    (car (quote ()))))
+
+(define *lambda
+  (lambda (e table)
+    (build (quote non-primitive)
+           (cons table (cdr e)))))
+
+(define table-of first)
+(define formals-of second)
+(define body-of third)
+
+(define evcon
+  (lambda (lines table)
+    (cond
+      ((else? (question-of (car lines)))
+       (meaning (answer-of (car lines)) table))
+      ((meaning (question-of (car lines)) table)
+       (meaning (answer-of (car lines)) table))
+      (else (evcon (cdr lines) table)))))
+
+(define question-of first)
+(define answer-of second)
+
+(define else?
+  (lambda (x)
+    (cond
+      ((atom? x) (eq? x (quote else)))
+      (else #f))))
+
+(define *cond
+  (lambda (e table)
+    (evcon (cond-lines-of e) table)))
+
+
+(define cond-lines-of cdr)
+
+(*cond '(cond (coffee klatsch)(else party)) '(((coffee)(#t))((klatsch party)(5 (6)))))
+
+(define evlis
+  (lambda (args table)
+    (cond
+      ((null? args) (quote ()))
+      (else (cons (meaning (car args) table) (evlis (cdr args) table))))))
+
+(define *application
+  (lambda (e table)
+    (apply
+      (meaning (function-of e) table)
+      (evlis (arguments-of e) table))))
+
+(define function-of car)
+(define arguments-of cdr)
+
+(define primitive?
+  (lambda (l)
+    (eq? (first l) (quote primitive))))
+
+
+(define non-primitive?
+  (lambda (l)
+    (eq? (first l) (quote non-primitive))))
+
+(define apply
+  (lambda (fun vals)
+    (cond
+      ((primitive? fun)
+       (apply-primitive (second fun) vals))
+      ((non-primitive? fun)
+       (apply-primitive (second fun) vals)))))
+
+(define apply-primitive
+  (lambda (name vals)
+    (cond
+      ((eq? name (quote cons))
+       (cons (first vals) (second vals)))
+      ((eq? name (quote car))
+       (car (first vals)))
+      ((eq? name (quote cdr))
+       (cdr (first vals)))
+      ((eq? name (quote null?))
+       (null? (first vals)))
+      ((eq? name (quote eq?))
+       (eq? (first vals) (second vals)))
+      ((eq? name (quote atom?))
+       (atom? (first vals)))
+      ((eq? name (quote number?))
+       (number? (first vals)))
+      )))
+
+(define apply-closure
+  (lambda (closure vals)
+    (meaning (body-of closure)
+             (extend-table (new-entry (formals-of closure) vals) (table-of
+                                                                   closure)))))
